f2538c769f937bf6ea83c8f2ca0bf0a44f90d4cb
hwiy01 <vdocom02@naver.com>
2024-11-10 00:34:01 +0900

    design: 너비 고정하고  로그인 화면 배경색 설정

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Login = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    width: 100%;
+    background-color: #14532D;
+    height: 100vh;
+`
