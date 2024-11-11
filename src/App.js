d9d41c1c93383ab86f379833b9faf233a40a5bf5
변현석 <bhs3066@gmail.com>
2024-11-11 16:41:19 +0900

    feat : TopNavBack 제작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import Home from "./pages/homepage/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppDom>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </AppDom>
   );
 }
 
 export default App;
+
+const AppDom = styled.div`
+  width: min(100vw, 600px); // 화면 너비에 맞추면서 최대 600px로 제한
+  height: 100vh; // 웹 뷰
+  margin: 0 auto;
+
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  @media (max-width: 600px) {
+    width: 100vw;
+    height: calc(var(--vh, 1vh) * 100);
+  }
+`;
